db017b9f906fafd1427a2d32593b881ce8df9190
Wouter Devriese <wouter.devriese@imec.be>
2021-05-04 16:13:00 +0200

    updated for multicontainer setup and alsa-utils

--- run.sh
+++ run.sh
@@ -4,18 +4,15 @@ OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
 echo "OS Version is $OS_VERSION"
 
 cd output
-mod_dir="example_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
-# mod_dir="i2s_mic_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+# mod_dir="example_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+mod_dir="i2s_mic_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
 for each in $mod_dir; do
 	echo Loading module from "$each"
-	insmod "$each/hello.ko"
-	# insmod "$each/snd-i2smic-rpi.ko"
-	lsmod | grep hello
-	# lsmod | grep snd-i2smic-rpi
-	rmmod hello
+	# insmod "$each/hello.ko"
+	insmod "$each/snd-i2smic-rpi.ko" rpi_platform_generation=2
+	# lsmod | grep hello
+	lsmod | grep snd-i2smic-rpi
+	# rmmod hello
 	# rmmod snd-i2smic-rpi
 done
 
-while true; do
-	sleep 60
-done
